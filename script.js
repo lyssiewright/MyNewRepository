ff2b4c53e05af4534fd28b034d83d5c193151921
Lyssie Wright <lyssiewright.15@gmail.com>
2022-01-22 00:54:58 -0600

    Increased header size and added links to navigate down page

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*const h1 = document.createElement("h1");
-h1.textContent = "LYSSIE WRIGHT";
-document.querySelector("body").appendChild(h1);
-
-const h2 = document.createElement("h2");
-h2.textContent = "(Pronounced LI-SEE)";
-document.querySelector("body").appendChild(h2);*/
 
 function clickFunction1(){
     document.getElementById("Metalsa-exp").innerHTML = "I was an intern during the spring semester my senior year (2018).";    
@@ -15,3 +8,51 @@ function clickFunction2(){
 function clickFunction3(){
     document.getElementById("Leonardo-DRS-exp").innerHTML = "DRS is a government defense contractor. I was a Manufacturing Engineer for just over a year and a half. It was after this job that I decided manufacturing was not for me.";
 }
+
+
+
+function toggleText() {
+  
+            // Get all the elements from the page
+    const Metalsa = 
+        document.getElementById("Metalsa");
+        const showMoreText =
+            document.getElementById("moreText");
+  
+        const buttonText =
+            document.getElementById("textButton");
+  
+            // If the display property of the dots 
+            // to be displayed is already set to 
+            // 'none' (that is hidden) then this 
+            // section of code triggers
+        if (Metalsa.style.display === "none") {
+  
+                // Hide the text between the span
+                // elements
+            showMoreText.style.display = "none";
+  
+                // Show the dots after the text
+            Metalsa.style.display = "inline";
+  
+                // Change the text on button to 
+                // 'Show More'
+            buttonText.innerHTML = "Show More";
+            }
+  
+            // If the hidden portion is revealed,
+            // we will change it back to be hidden
+        else {
+  
+                // Show the text between the
+                // span elements
+             showMoreText.style.display = "inline";
+  
+                // Hide the dots after the text
+             Metalsa.style.display = "none";
+  
+                // Change the text on button
+                // to 'Show Less'
+             buttonText.innerHTML = "Show Less";
+            }
+        }
