30f83f72a9e3cad95d3c17c0bff48f06c1fc3160
Lyssie Wright <lyssiewright.15@gmail.com>
2022-01-21 22:24:21 -0600

    add work experience and work experience details with button push

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "(Pronounced LI-SEE)";
 document.querySelector("body").appendChild(h2);*/
 
-
-
+function clickFunction1(){
+    document.getElementById("Metalsa-exp").innerHTML = "I was an intern during the spring semester my senior year (2018).";    
+}
+function clickFunction2(){
+    document.getElementById("Univance-exp").innerHTML = "Univance is another automotive factory. I was a Manufacturing Engineer for 2 years before I decided I wanted to move out of Kentucky.";
+}
+function clickFunction3(){
+    document.getElementById("Leonardo-DRS-exp").innerHTML = "DRS is a government defense contractor. I was a Manufacturing Engineer for just over a year and a half. It was after this job that I decided manufacturing was not for me.";
+}
